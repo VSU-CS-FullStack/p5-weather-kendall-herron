37a4bb15406bd8190b64861daf3a723fcbf45fd4
kendallherron <ktherron@valdosta.edu>
2018-09-25 13:31:26 -0400

    Set up React Components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import react from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import { Provider } from "react-redux";
+
 import './index.css';
+
 import App from "./components/app";
 import rootReducer from "./reducers";
-import registerServiceWorker from './registerServiceWorker';
+
 import'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+	rootReducer,
+	{},
+	composeWithDevTools(applyMiddleware())
+);
+
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
