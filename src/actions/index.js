d0d3b572e1e545e84d83c9b59e2ca67593ac02b1
kendallherron <ktherron@valdosta.edu>
2018-09-25 21:00:54 -0400

    Ajax requests without asynchronicity

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
+
 const API_KEY = "af34bf3c8546fcd5bf929adeb4b3c0cb";
+const ROOT_URL =
+`https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+	const url =`${ROOT_URL}&q=${city},us`;
+
+	// axios.get(url) returns a promise
+	const request = axios.get(url);
+
+	return {        
+		type: FETCH_WEATHER,        
+		payload: request
+	}
+}
