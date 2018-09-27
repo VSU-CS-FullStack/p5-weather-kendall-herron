d92dfa8272a35a1b385c47110a29b912f4b797e1
kendallherron <ktherron@valdosta.edu>
2018-09-26 22:09:48 -0400

    created chart.js

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,49 +1,29 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from "react-sparklines";
+// import { Sparklines, SparklinesLine } from "react-sparklines";
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
 	renderWeather(cityData){
 		const name = cityData.city.name;
 
-		const samples = [288.15, 285.32, 283.49, 282.89, 283.465, 290.566, 294.232, 294.472, 290.084, 287.118, 285.049];
-		const samples2 = [1022.38, 1022.73, 1022.47, 1021.74, 1021.67, 1021.23, 1019.67, 1018.25, 1017.83, 1017.92, 1017.29];
-		const samples3 = [65, 80, 95, 100, 68, 54, 52, 65, 74, 88]
+		const temperatures = [288.15, 285.32, 283.49, 282.89, 283.465, 290.566, 294.232, 294.472, 290.084, 287.118, 285.049]
+		const pressures = [1022.38, 1022.73, 1022.47, 1021.74, 1021.67, 1021.23, 1019.67, 1018.25, 1017.83, 1017.92, 1017.29]
+		const humiditys = [65, 80, 95, 100, 68, 54, 52, 65, 74, 88]
 	
 
 		return (
 			<tr key={ name }>
 				<td>{name} </td>
 				<td>
-					<div>
-						<Sparklines
-							svgHeight={120}
-							svgWidth={180}
-							data={samples}>
-							<SparklinesLine color="red" />
-						</Sparklines>
-					</div>
+					<Chart data={temperatures} color="red" units="Kelvin"/>
 				</td>
 				<td>
-					<div>
-						<Sparklines
-							svgHeight={120}
-							svgWidth={180}
-							data={samples2}>
-							<SparklinesLine color="blue" />
-						</Sparklines>
-					</div>
+					<Chart data={pressures} color="blue" units="hPa"/>
 				</td>
 				<td>
-					<div>
-						<Sparklines
-							svgHeight={120}
-							svgWidth={180}
-							data={samples3}>
-							<SparklinesLine color="green" />
-						</Sparklines>
-					</div>
+					<Chart data={humiditys} color="green" units="%"/>
 				</td>
 			</tr>
 		)
